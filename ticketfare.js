3af0d4e521c31d4aa0b760d84c7e6d08ad542186
sabbir349 <sabbirhossainnh@gmail.com>
2024-11-04 22:39:12 +0600

    first correction

--- ticketfare.js
+++ ticketfare.js
@@ -1,19 +1,19 @@
 var money = 1000;
 var ticket = 800;
-var age = 42;
+var age = 62;
 
 if(age < 10){
     console.log('free for children')
 }
 else if(age > 10 && age < 33){
     var discount = ticket * 50/100;
-    var pay_able_money_for_student = ticket - discount
-    console.log(pay_able_money_for_student)
+    var pay_able_money = ticket - discount
+    console.log(pay_able_money , "for students")
 }
 else if(age >= 60){
     var discount = ticket * 15/100;
-    var pay_able_money_for_senior_citizen = ticket - discount
-    console.log(pay_able_money_for_senior_citizen)
+    var pay_able_money = ticket - discount
+    console.log(pay_able_money , "for senior citizens")
 }
 else{
     console.log(ticket)
